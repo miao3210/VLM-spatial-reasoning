2e3b9861380541dda5fbe0a82e28a3131d4fa036
miao3210 <1875696231@qq.com>
2025-07-08 16:26:45 -0700

    add road layout sampler with label as TODO

--- spatok/dynamic_benchmark/__init__.py
+++ spatok/dynamic_benchmark/__init__.py
@@ -2,6 +2,6 @@ import os
 import pdb
 import warnings
 
-from spatok.dynamic_benchmark.Simple.dynamic_sampler import DynamicSampler
-from spatok.dynamic_benchmark.OpenScenarioStyle.roadlayout_dynamic_sampler import RoadLayoutDynamicSampler
+from spatok.dynamic_benchmark.simple.dynamic_sampler import DynamicSampler
+from spatok.dynamic_benchmark.amas_style.roadlayout_dynamic_sampler import RoadLayoutDynamicSampler
 from spatok.dynamic_benchmark.utils import *
